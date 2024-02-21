3eafd1d9a14f610367bdb600199460e19017c0dc
cmeowwww <a3508178@gmail.com>
2024-02-21 11:41:31 +0800

    week01

---
+++ week01/week01-3.cpp
@@ -0,0 +1,9 @@
+//瘋狂程設 F001 兩數相加
+
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d",a+b); 
+}
---
+++ week01/week01-3.py
@@ -0,0 +1,5 @@
+#瘋狂程設 F001 兩數相加
+
+a=list(map(int,input().split()))
+
+print(sum(a),end='')
