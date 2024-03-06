e9810eb21b3e75a23443a368fbed551bd8638502
cmeowwww <a3508178@gmail.com>
2024-03-06 10:30:55 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,9 @@
+//要寫文字筆記的C++程式碼,文字裡,
+# include <stdio.h>
+int main()
+
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  printf("%d",a+b);
+}
---
+++ week03/week03-1.py
@@ -0,0 +1,10 @@
+#(+程式碼) 寫python程式 week03-2
+#也想要做[直式加法]了解 f格式
+
+print('請輸入2個數字:')
+a,b=list(map(int,input().split() ))
+print(f'{a:5}')
+print(f'{b:5}')
+print('-------')
+
+print(f'a+b:5')
