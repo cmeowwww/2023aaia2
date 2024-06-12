76a040dea980519177aaac3c01e262bec25dea08
cmeowwww <144758517+cmeowwww@users.noreply.github.com>
2024-06-12 12:00:50 +0800

    Add files via upload

---
+++ week17/week17-5a.cpp
@@ -0,0 +1,18 @@
+///(SOIT107_ADVANCE_005) 進階題：字串長度
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char a[100],b[100];
+	scanf("%s",a);
+	scanf("%s",b);
+	int L1=strlen(a);
+	int L2=strlen(b);
+
+	if(L1>L2) printf("1");
+	else if(L1<L2) printf("-1");
+	else{
+		printf("%d",strcmp(a,b));
+	}
+
+}
